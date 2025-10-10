1be95fdd4bdf7bfea648963406f5590fa0b69e87
chrisghill <chris@massdriver.cloud>
2025-10-09 18:16:58 -0600

    remove references to v0 bundles

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,9 +3,4 @@ set -eo pipefail
 
 xo deployment start
 
-if [[ -n "${MASSDRIVER_BUNDLE_VERSION:-}" ]]; then
-  xo bundle pull
-else
-  xo bundle pullv0
-  tar -xzf bundle.tar.gz
-fi
+xo bundle pull
